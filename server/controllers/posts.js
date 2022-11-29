347073de91b0bf516051b03d4c88a7628d90de73
JoffreyNK <joyjoffrey@gmail.com>
2022-11-29 09:38:37 +0200

    create controller for getting all posts and adding posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,10 +1,27 @@
-export const getPosts = (req, res) => {
-    res.send('all posts retrieved')
+import postMessage from '../models/postMessage.js'
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await postMessage.find();
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
 }
 
 
-export const createPost = (req, res) => {
-    res.send('post created')
+export const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new postMessage(post)
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
 }
 
 export const getPost = (req, res) => {
