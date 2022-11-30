919808cacb9d61e57823cbb4a5c84dcac20efe38
JoffreyNK <joyjoffrey@gmail.com>
2022-11-30 12:19:09 +0200

    add action creator methods

--- client/src/redux/actioncreators/postActionCreators.js
+++ client/src/redux/actioncreators/postActionCreators.js
@@ -5,8 +5,54 @@ const URL = 'http://localhost:3001/posts'
 import { CREATE_POST, GET_POST, GET_POSTS, DELETE_POST, UPDATE_POST } from "../actions/PostActions";
 
 const createPost = createAsyncThunk(CREATE_POST, async (params) => {
-  
-  const post = await fetch(URL, params)
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(params),
+  };
+  const post = await fetch(URL, options);
+  return post.json();
+})
+const getPosts = createAsyncThunk(GET_POSTS, async () => {
+  const posts = await fetch(URL);
+  return posts.json();
 })
 
 
+const updatePost = createAsyncThunk(UPDATE_POST, async (params) => {
+  const options = {
+    method: 'PATCH',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(params),
+  };
+  const updatedpost = await fetch(URL, options)
+  return updatedPost.json();
+} )
+
+const getpost = createAsyncThunk(GET_POST, async (param) => {
+  const option = {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(param)
+  }
+  const getsinglepost = await fetch(URL, option)
+  return getsinglepost.json();
+})
+
+const deletepost = createAsyncThunk(DELETE_POST, async (param) => {
+  const option = {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(param)
+  }
+  const getsinglepost = await fetch(URL, option)
+  return getsinglepost.json();
+})
